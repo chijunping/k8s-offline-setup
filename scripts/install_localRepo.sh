691cc284c133904dab3666bf8ce0c3e0e9e891e6
chijunping <378344307@qq.com>
2021-07-08 15:56:26 +0800

    初始化

---
+++ scripts/install_localRepo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rpm -ivh ../packages/deltarpm*
+rpm -ivh ../packages/libxml2-python*
+rpm -ivh ../packages/python-deltarpm*
+rpm -ivh ../packages/createrepo*
+
+createrepo  /root/k8sOfflineSetup/packages
+
+# 备份现有源
+if [ -f "/etc/yum.repos.d/CentOS-Base.repo" ];then
+    mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo_bak_$(date "+%Y-%m-%d_%H-%M-%S")
+fi
+cp -f ../repos/CentOS-Media.repo /etc/yum.repos.d/
